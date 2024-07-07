3ff4ec02286ea6fda6ad6e21c8ccc44e2a35105a
moemeylane <wangarimaureen668@gmail.com>
2024-07-07 18:54:10 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(records) {
+    const record = [
+        { year: "2015", result: "W"},
+        { year: "2014", result: "N/A"},
+        { year: "2013", result: "L"},
+    ] 
+ const winRecord = records.find(record => record.result === "W");
+    
+    if (winRecord) {
+        return winRecord.year;
+    }
+    
+    return undefined;
+}
